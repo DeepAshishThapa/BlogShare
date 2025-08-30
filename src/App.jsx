eab9525b50dc013f5d787a77b793bc533f72f3d2
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-08-30 19:43:23 +0930

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,14 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  
+   
+
 
   return (
+      
     <>
       <div className='h-3 bg-red-500'>hi</div>
 
