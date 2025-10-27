99d841618704817d919f33d4d8751ed0971d7cac
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-10-27 20:22:45 +1030

    Added TinyMCE Cloud API key to enable editor features

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -9,9 +9,9 @@ export default function RTE({ control, name, defaultValue = "", label }) {
                 <Box
                     component="label"
                     sx={{
-                        display:"inline-block",
-                        mb:1,
-                        pl:1
+                        display: "inline-block",
+                        mb: 1,
+                        pl: 1
 
 
                     }}
@@ -28,24 +28,28 @@ export default function RTE({ control, name, defaultValue = "", label }) {
                 render={({ field: { onChange, value } }) => (
 
                     <Editor
-                        apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
-                        value={value}
+                        apiKey='7fftz4fxet391uev8nhcagzn9ahsta2yzdx3njpkl6zd3e37'
+                         value={value}  
+                         onEditorChange={onChange} 
                         init={{
-                            height: 500,
-                            menubar: false,
                             plugins: [
-                                "advlist autolink lists link image charmap print preview anchor",
-                                "searchreplace visualblocks code fullscreen",
-                                "insertdatetime media table paste code help wordcount"
+                                // Core editing features
+                                'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+                                // Your account includes a free trial of TinyMCE premium features
+                                // Try the most popular premium features until Nov 10, 2025:
+                                'checklist', 'casechange', 'formatpainter', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown', 'importword', 'exportword', 'exportpdf'
                             ],
-                            toolbar:
-                                "undo redo | formatselect | " +
-                                "bold italic backcolor | alignleft aligncenter " +
-                                "alignright alignjustify | bullist numlist outdent indent | " +
-                                "removeformat | help",
-                            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+                            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography uploadcare | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                            tinycomments_mode: 'embedded',
+                            tinycomments_author: 'Author name',
+                            mergetags_list: [
+                                { value: 'First.Name', title: 'First Name' },
+                                { value: 'Email', title: 'Email' },
+                            ],
+                            ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+                            uploadcare_public_key: '78450930a6481309eaa3',
                         }}
-                        onEditorChange={onChange}
+                        initialValue="Anything in your mind?"
                     />
                 )}
             />
