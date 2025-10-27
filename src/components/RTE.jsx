d0d74442e50daf992e5c71b97aa49657c4384fed
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-10-27 17:39:13 +1030

    added label for the editor

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,34 +1,55 @@
+import { Box } from "@mui/material";
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
 
-export default function RTE({ control, name, defaultValue = ""}){
+export default function RTE({ control, name, defaultValue = "", label }) {
     return (
-        <Controller
-            name={name||"content"}
-            control={control}
-            defaultValue={defaultValue}
-            render={({ field: { onChange, value } }) => (
-                <Editor
-                    apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
-                    value={value} 
-                    init={{
-                        height: 500,
-                        menubar: false,
-                        plugins: [
-                            "advlist autolink lists link image charmap print preview anchor",
-                            "searchreplace visualblocks code fullscreen",
-                            "insertdatetime media table paste code help wordcount"
-                        ],
-                        toolbar:
-                            "undo redo | formatselect | " +
-                            "bold italic backcolor | alignleft aligncenter " +
-                            "alignright alignjustify | bullist numlist outdent indent | " +
-                            "removeformat | help",
-                        content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+        <>
+            {label &&
+                <Box
+                    component="label"
+                    sx={{
+                        display:"inline-block",
+                        mb:1,
+                        pl:1
+
+
                     }}
-                    onEditorChange={onChange}
-                />
-            )}
-        />
+
+                >
+                    {label}
+                </Box>
+
+            }
+            <Controller
+                name={name || "content"}
+                control={control}
+                defaultValue={defaultValue}
+                render={({ field: { onChange, value } }) => (
+
+                    <Editor
+                        apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
+                        value={value}
+                        init={{
+                            height: 500,
+                            menubar: false,
+                            plugins: [
+                                "advlist autolink lists link image charmap print preview anchor",
+                                "searchreplace visualblocks code fullscreen",
+                                "insertdatetime media table paste code help wordcount"
+                            ],
+                            toolbar:
+                                "undo redo | formatselect | " +
+                                "bold italic backcolor | alignleft aligncenter " +
+                                "alignright alignjustify | bullist numlist outdent indent | " +
+                                "removeformat | help",
+                            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+                        }}
+                        onEditorChange={onChange}
+                    />
+                )}
+            />
+        </>
     )
 }
+
