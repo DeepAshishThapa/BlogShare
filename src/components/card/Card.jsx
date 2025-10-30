e8c15b48dec546b57a13e28fa62eed0666045b97
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-10-31 09:58:43 +1030

    fixed image rendering using getFileView instead of getFilePreview

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,25 +5,39 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import postService from '@/Appwrite/post/api';
-export default function MediaCard({title,content,featuredImage}) {
+import postService from '../../Appwrite/post/api';
+import { useState,useEffect } from 'react';
+export default function MediaCard({ post }) {
+  const [imgUrl, setImgUrl] = useState("");
+
+  useEffect(() => {
+    let cancelled = false;
+    (async () => {
+      const url = await postService.getfilepreview(post.featuredImage);
+      if (!cancelled) setImgUrl(url);
+    })();
+    return () => { cancelled = true; };
+  }, [post.featuredImage]);
+
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
+        component="img"
         sx={{ height: 140 }}
-        image={postService.getfilepreview(featuredImage)}
+        image={imgUrl}
         title="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {title}
+          {post.title}
         </Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          {content}
+          {post.content}
         </Typography>
       </CardContent>
       <CardActions>
-        
+
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
