90ba589745e243e3e73b695d4c134f49efd55814
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-10-31 12:54:10 +1030

    added conditional rendering check for pots array

--- src/pages/Allposts.jsx
+++ src/pages/Allposts.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
-import MediaCard from "../components/card/Card.jsx"
+import Card from "../components/card/Card.jsx"
 import { Box, Typography } from '@mui/material'
 import { Container } from '@mui/material'
+import { useState } from 'react'
+import postService from '../Appwrite/post/api.js'
+import { useEffect } from 'react'
 
 function Allposts() {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    postService.getposts()
+      .then((res) => {
+        console.log("Fetched posts:", res);
+        if (res && res.rows) {
+          setPosts(res.rows);
+        } 
+      })
+      
+  }, []);
+
   return (
     <div>
       <Box
         sx={{
           backgroundImage: 'linear-gradient(to right, #0a1f44 0%, #1d3989 50%, #5873bc 80%)',
           color: 'white',
-          py: { xs: 5, sm: 5},
+          py: { xs: 5, sm: 5 },
 
         }}
       >
@@ -40,8 +55,16 @@ function Allposts() {
 
       </Box>
 
+      {posts && posts.length > 0 ? (
+        posts.map((post) => (
+          <Card key={post.$id} post={post} />
+        ))
+      ) : (
+        <p>No posts found.</p>
+      )}
 
-      <MediaCard />
+    
+
 
     </div>
   )
