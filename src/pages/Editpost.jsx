96ee22bc991e91583acbb40e40ff89c625db5f57
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-11-02 11:01:01 +1030

    added editpost page for each post

---
+++ src/pages/Editpost.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import PostForm from '@/components/post-form/PostForm'
+import { useState } from 'react'
+import { useNavigate, useParams } from 'react-router'
+import postService from '@/Appwrite/post/api'
+
+
+
+
+function Editpost() {
+    const [post, setPosts] = useState(null)
+    const { slug } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (slug) {
+            postService.getpost(slug).then((post) => {
+                if (post) {
+                    setPosts(post)
+                }
+            })
+        } else {
+            navigate('/')
+        }
+
+
+    }, [slug, navigate])
+
+    return post ? (
+        <>
+            <Box
+                sx={{
+
+                    color: 'white',
+                    py: { xs: 5, sm: 5 },
+
+                }}
+            >
+                <PostForm post={post}/>
+            </Box>
+
+
+
+        </>
+    ):null
+}
+
+export default Editpost
