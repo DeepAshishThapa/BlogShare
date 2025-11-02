7ed10ed957b44b53b8e135e97ec87cf5c5713899
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-11-02 11:05:38 +1030

    fully made Allposts page to render all the posts

--- src/pages/Allposts.jsx
+++ src/pages/Allposts.jsx
@@ -1,23 +1,10 @@
 import React from 'react'
 import {Box} from '@mui/material'
-import { useState } from 'react'
-import postService from '../Appwrite/post/api.js'
-import { useEffect } from 'react'
 import AllpostsHero from '@/components/Allposts/AllpostsHero.jsx'
 import AllpostsSection from '@/components/Allposts/AllpostsSection.jsx'
 
 function Allposts() {
-  const [posts, setPosts] = useState([])
-  useEffect(() => {
-    postService.getposts()
-      .then((res) => {
-        console.log("Fetched posts:", res);
-        if (res && res.rows) {
-          setPosts(res.rows);
-        }
-      })
 
-  }, []);
 
   return (
     <>
