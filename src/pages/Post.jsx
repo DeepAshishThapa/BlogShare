6a8135aae89f82cab9d59ad0ac791af918822cb0
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-11-02 13:41:01 +1030

    Added post page that fetches displays a post by slug

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -4,6 +4,8 @@ import postService from '@/Appwrite/post/api'
 import { Link, useNavigate, useParams } from 'react-router'
 import parse from "html-react-parser"
 import { useSelector } from 'react-redux'
+import { Container, Box } from '@mui/material'
+
 
 
 
@@ -12,20 +14,85 @@ function Post() {
     const { slug } = useParams()
     const [post, setpost] = useState(null)
     const navigate = useNavigate()
+    const [imgUrl, setImgUrl] = useState('')    
 
     const userData = useSelector((state) => state.auth.userData)
 
     const isAuthor = post && userData ? post.userId === userData.$id : false;
 
-    
+    useEffect(() => {
+        if (slug) {
+            postService.getpost(slug).then((post) => {
+                if (post) {
+                    setpost(post)
+
+                }
+                else {
+                    navigate("/")
+                }
 
 
 
-    return (
+            })
+        }
+
+    }, [slug, navigate])
+
+    useEffect(() => {
+        if (!post?.featuredImage) return
+        let cancelled = false
+            ; (async () => {
+                const url = await postService.getfilepreview(post.featuredImage)
+                if (!cancelled) setImgUrl(url)
+            })()
+        return () => { cancelled = true }
+    }, [post?.featuredImage])
+
+
+
+    const deletepost = () => {
+        postService.deletepost(slug).then((status) => {
+            if (status) {
+                postService.deletefile(post.featuredImage)
+                navigate("/")
+            }
+        })
+
+    }
+
+
+
+
+
+
+
+
+    return post ? (
         <>
+            <Container maxWidth="md">
+                <Box
+                    component="img"
+                    src={imgUrl}
+                    alt={post.title}
+                >
+
+
+
+                </Box>
+                <Box>
+                    <Box>
+                        {post.title}
+                    </Box>
+                    <Box>
+                       { parse(post.content)}
+                    </Box>
+                </Box>
+
+            </Container>
+
 
         </>
-    )
+    ) : null
 }
 
 export default Post
