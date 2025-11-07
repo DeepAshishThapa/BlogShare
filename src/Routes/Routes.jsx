4e85d7ffad745744046958a928b3e5f2b5050829
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-11-07 14:18:57 +1030

    integrated RequireAuth guard into protected routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,4 @@
-import { Routes ,Route} from "react-router";
+import { Routes, Route } from "react-router";
 import Mainlayout from "@/pages/Mainlayout";
 import Loginpage from "@/pages/Loginpage";
 import Signuppage from "@/pages/Signuppage";
@@ -6,6 +6,7 @@ import Allposts from "@/pages/Allposts";
 import Addposts from "@/pages/Addposts";
 import Editpost from "@/pages/Editpost";
 import Post from "@/pages/Post";
+import RequireAuth from "@/pages/RequireAuth";
 
 
 import Homepage from "@/pages/Homepage";
@@ -13,19 +14,26 @@ import Homepage from "@/pages/Homepage";
 function AppRoutes() {
   return (
     <>
-    <Routes>
-        <Route path="/" element={<Mainlayout/>}>
-           <Route index element={<Homepage/>}/>
-           <Route path="login" element={<Loginpage/>}/>
-           <Route path="signup" element={<Signuppage/>}/>
-           <Route path="all-posts" element={<Allposts/>}/>
-           <Route path="add-posts" element={<Addposts/>}/>
-           <Route path="edit-post/:slug" element={<Editpost/>}/>
-           <Route path="post/:slug" element={<Post/>}/>
+      <Routes>
+        <Route path="/" element={<Mainlayout />}>
+          {/* Public */}
+          <Route index element={<Homepage />} />
+          <Route path="login" element={<Loginpage />} />
+          <Route path="signup" element={<Signuppage />} />
+          <Route path="all-posts" element={<Allposts />} />
+          <Route path="post/:slug" element={<Post />} />
+
+          {/* Protected */}
+          <Route element={<RequireAuth/>}>
+          <Route path="add-posts" element={<Addposts/>}/>
+          <Route path="edit-post/:slug" element={<Editpost/>}/>
+           
+          </Route>
 
 
         </Route>
-    </Routes>
+
+      </Routes>
     </>
   )
 }
