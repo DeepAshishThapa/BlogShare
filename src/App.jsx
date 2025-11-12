d1b623267cf8d2b3e69c0dff623622abdccdfa12
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-11-12 19:04:50 +1030

    restored session

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import { login, logout } from './Appwrite/auth/authSlice'
 import { useDispatch } from 'react-redux'
 import authService from './Appwrite/auth/auth'
 
-import { AppBar, Box, Grid } from '@mui/material'
-
-
 import Login from './components/authcomp/Login'
 import AppRoutes from './Routes/Routes'
 
@@ -16,37 +13,12 @@ import AppRoutes from './Routes/Routes'
 
 function App() {
   const dispatch = useDispatch();
-  // const [loading, setLoading] = useState(true)
-  // const dispatch = useDispatch()
-
-  // useEffect(() => {
-
-  //   authService.getAccount()
-  //     .then((userData) => {
-  //       if (userData) {
-  //         dispatch(login(userData))
-
-  //       }
-  //       else {
-  //         dispatch(logout())
-  //       }
-
-
-  //     })
-  //     .finally(() => setLoading(false))
-
-
-
-
-
-  // }, [])
-
-
+  
   useEffect(() => {
     authService.getAccount()
       .then((userData) => {
         if (userData) {
-          dispatch(login(userData)); // ✅ restore session
+          dispatch(login(userData)); //  restore session
         } else {
           dispatch(logout());
         }
@@ -72,10 +44,7 @@ function App() {
 
     <>
 
-      {/* <AppBar position='sticky'>
-          <Toolbar>My App</Toolbar>
-          
-        </AppBar> */}
+  
 
 
 
