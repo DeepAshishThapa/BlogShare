930aa4435e55923d31250f180479549ec8eb6116
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-11-15 20:14:44 +1030

    added tag-based post filtering with UI and fetch logic

--- src/components/Allposts/AllpostsSection.jsx
+++ src/components/Allposts/AllpostsSection.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import postService from '../../Appwrite/post/api.js'
 import MediaCard from '../card/Card.jsx';
 import { Container, Box, CircularProgress } from '@mui/material';
+import TagsButtons from './TagsButtons.jsx';
 
 
 /**
@@ -15,24 +16,68 @@ function AllpostsSection() {
 
     const [posts, setPosts] = useState([])             // Stores all fetched posts
     const [loading, setloading] = useState(true)        // Controls the loading spinner visibility
+    const [selectedTag, setselectedTag] = useState('All')  //stores the current active tag
+
+    const tags = [
+        'All',
+        'Career & Learning',
+        'Project Building',
+        'Roadmap',
+        'Resume',
+        'Web development',
+        'AI & Data',
+        'Remote Jobs',
+        'Portfilio',
+        'Job Search',
+        'Cloud & Devops',
+    ]
 
 
 
-    // ---------- Fetch posts on component mount ----------
+    // ---------- Fetch posts whenever selectedTag changes ----------
     useEffect(() => {
-        postService.getposts()
-            .then((res) => {
-                if (res && res.rows) {
-                    setPosts(res.rows);
-                }
-            })
-            .finally(() => setloading(false))
+        async function fetchposts() {
+            setloading(true)
 
-    }, []);
-    
+            try {
+                let res
+                if (selectedTag == 'All') {
+
+                    // fetch all posts
+                    res = await postService.getposts()
+                }
+                else {
+                    // fetch posts by single tag
+                    res = await postService.gettagsposts(selectedTag)
+                }
+
+                if (res && res.rows) {
+                    setPosts(res.rows)
+                }
+                else {
+                    setPosts([]);
+                }
+            }
+            finally {
+                setloading(false)
+            }
+
+        }
+        fetchposts();
+    }, [selectedTag]);
+
     return (
         <>
+
             <Container maxWidth="md" sx={{ mt: 10 }}>
+
+                {/* Tag buttons */}
+                <TagsButtons
+                    tags={tags}
+                    selectedTag={selectedTag}
+                    onTagChange={setselectedTag}
+                />
+
                 {
                     loading ? (
                         <Box sx={{
