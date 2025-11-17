1fb77bbfd08e25e03f5f4921bab55a8abc04c2ce
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-11-17 19:17:06 +1030

    added table for comments section

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,7 @@ const config={
     appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteTableId:String(import.meta.env.VITE_APPWRITE_TABLE_ID),
+    appwriteCommentsTableId:String(import.meta.env.VITE_APPWRITE_COMMENTSTABLE_ID),
     appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 
 
